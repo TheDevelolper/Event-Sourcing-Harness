527bdefdb5b95a67b98dff0195ab23406e42e08d
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2025-09-04 14:06:36 +0100

    feat(config): Adds feature toggles from shared appsettings

---
+++ SaasFactory.Shared.Config/BuilderExtensions.cs
@@ -0,0 +1,26 @@
+﻿
+using Aspire.Hosting;
+using Microsoft.Extensions.Configuration;
+
+namespace SaasFactory.Shared.Config;
+
+public static class ConfigurationManagerExtensions
+{
+
+    public static DistributedApplicationBuilder UseSharedConfiguration(this DistributedApplicationBuilder builder)
+    {
+        UseSharedConfiguration(builder.Configuration);
+        return builder;
+    }
+    
+    public static IConfigurationBuilder UseSharedConfiguration(this IConfigurationBuilder builderConfiguration)
+    {
+        // Loads cross project shared configuration
+        
+        builderConfiguration
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("appsettings.shared.json", optional: true); 
+
+        return builderConfiguration;
+    }
+}
