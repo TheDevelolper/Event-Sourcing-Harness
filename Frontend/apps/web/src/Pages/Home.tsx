cc850213e3371b89a26c6449170c62438fd0e071
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2025-10-07 23:41:06 +0100

    style(menu): Small menu styling improvements

--- Frontend/apps/web/src/Pages/Home.tsx
+++ Frontend/apps/web/src/Pages/Home.tsx
@@ -2,7 +2,7 @@ import { RestaurantMenu } from "@micro-frontends";
 
 export default function Home() {
   return (
-    <div className="max-w-[1080px] mx-auto mt-8">
+    <div className="max-w-[1250px] mx-auto mt-8 p-8">
       <RestaurantMenu></RestaurantMenu>
     </div>
   );
