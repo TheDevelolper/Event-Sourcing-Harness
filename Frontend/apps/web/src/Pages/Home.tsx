b5b12f3abec2320632d8c5db8a7774982e8fa981
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2025-10-07 23:28:07 +0100

    fix(style): Adds Home and dashboard layout fixes

--- Frontend/apps/web/src/Pages/Home.tsx
+++ Frontend/apps/web/src/Pages/Home.tsx
@@ -1,13 +1,15 @@
 export default function Home() {
   return (
-    <div className="text-center p-8 bg-gray-700">
-      <h1 className="text-3xl font-bold underline mb-4">Saas Factory UI</h1>
-      <p className="text-lg mb-2">
-        A page with more instructions will be coming here soon. Stay tuned!
-      </p>
-      <p className="text-sm">
-        You can visit the admin dashboard by heading over to /admin
-      </p>
+    <div className="w-[100vw]">
+      <div className="w-200 m-auto text-center p-8 bg-gray-700">
+        <h1 className="text-3xl font-bold underline mb-4">Saas Factory UI</h1>
+        <p className="text-lg mb-2">
+          A page with more instructions will be coming here soon. Stay tuned!
+        </p>
+        <p className="text-sm">
+          You can visit the admin dashboard by heading over to /admin
+        </p>
+      </div>
     </div>
   );
 }
