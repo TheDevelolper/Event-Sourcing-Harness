9ccb419d191f10b793b16934a178f3db1f672585
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2025-10-07 20:16:48 +0100

    feat(auth): Adds React protected routes

--- Frontend/apps/web/src/App.tsx
+++ Frontend/apps/web/src/App.tsx
@@ -1,44 +1,29 @@
-import { useState, useContext} from "react";
-import { KeycloakContext } from "./auth/keycloak-provider";
-
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Routes, Route, Link } from "react-router-dom";
+import { ProtectedRoute } from "./auth/protexted-route";
+import Admin from "./Pages/Admin";
+import Home from "./Pages/Home";
 
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const keycloak = useContext(KeycloakContext);
-
-  if (!keycloak) return null;
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline bg-blue-600">
-        Hello Tailwind
-      </h1>
+    <div>
+      <nav>
+        <Link to="/">Home</Link> | <Link to="/admin">Admin</Link>
+      </nav>
 
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/admin"
+          element={
+            <ProtectedRoute>
+              <Admin />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </div>
   );
 }
 
