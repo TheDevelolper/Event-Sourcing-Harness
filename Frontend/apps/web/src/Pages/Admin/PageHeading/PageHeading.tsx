a3f1453987200faafce3bec46f4ab6fe0a76025c
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2025-10-08 17:16:16 +0100

    style(admin): adjust layout spacing and positioning for headings and sidebar

--- Frontend/apps/web/src/Pages/Admin/PageHeading/PageHeading.tsx
+++ Frontend/apps/web/src/Pages/Admin/PageHeading/PageHeading.tsx
@@ -5,7 +5,7 @@ export interface PageHeadingProps {
 
 export function PageHeading(props: PageHeadingProps) {
   return (
-    <div className="flex md:items-center md:justify-between mt-4 ">
+    <div className="flex items-center md:items-center md:justify-between mt-16 lg:mt-4 mx-4">
       <div className="min-w-0 flex-1">
         <h2 className="text-2xl/7 font-bold text-gray-900 sm:truncate sm:text-3xl sm:tracking-tight">
           {props.title}
