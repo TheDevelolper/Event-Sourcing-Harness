8a2521f5f08f44ec4f842675be34e1d4abd6c4fa
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2025-11-09 16:05:15 +0000

    WIP user subscriptions

---
+++ Backend/Features/UserSubscriptions/SaasFactory.Features.UserSubscriptions.Tests/Specifications/PlaywrightStory.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Playwright;
+
+namespace Demo.Tests;
+
+internal abstract class PlaywrightStory
+{
+    protected readonly IPage Page;
+
+    protected PlaywrightStory(IPage page)
+    {
+        Page = page;
+    }
+}
