3e8190d1e063e1d74023b4c5c4b4ab975c408f5d
Kiran Randhawa <4026553+TheDevelolper@users.noreply.github.com>
2025-11-26 10:58:57 +0000

    Enhance subscription and order commands to include stream handling

--- Backend/Features/UserSubscriptions/SaasFactory.Features.UserSubscriptions.Contracts/Commands/PlaceOrderCommand.cs
+++ Backend/Features/UserSubscriptions/SaasFactory.Features.UserSubscriptions.Contracts/Commands/PlaceOrderCommand.cs
@@ -1,9 +1,6 @@
-
-using Mediator.Net.Contracts;
-
 namespace SaasFactory.Features.UserSubscriptions.Contracts.Commands;
 
 /// <summary>
 /// Command to place an order as part of the subscription process.
 /// </summary>
-public record PlaceOrderCommand() : ICommand;
+public record PlaceOrderCommand(List<string>? stream): BaseCommand(stream);
